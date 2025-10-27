bf7de30199aeaafcfab9e13932358033f9c7ab07
Nicolas Sanchez <nicopalas999@gmail.com>
2025-10-27 17:41:18 +0100

    Gamma flash preselection

--- Macros/gamma_flash.cpp
+++ Macros/gamma_flash.cpp
@@ -15,11 +15,11 @@ Int_t RunNumber, eventTime, BunchNumber, PSpulse;
 Double_t psTime;
 Double_t tof;
 Float_t PulseIntensity;
-Double_t tof0[5],tof1[5],tof2[5],tof3[5],tof4[5],tof5[5],tof6[5],tof7[5],tof8[5],tof9[5]; //must be hardcoded if arrays are defined at file scope
-Float_t amp0[5],amp1[5],amp2[5],amp3[5],amp4[5],amp5[5],amp6[5],amp7[5],amp8[5],amp9[5];
-Double_t new_tof0[5], new_tof1[5], new_tof2[5], new_tof3[5], new_tof4[5], new_tof5[5], new_tof6[5], new_tof7[5], new_tof8[5], new_tof9[5];
+Double_t tof0[20],tof1[20],tof2[20],tof3[20],tof4[20],tof5[20],tof6[20],tof7[20],tof8[20],tof9[20]; //must be hardcoded if arrays are defined at file scope
+Float_t amp0[20],amp1[20],amp2[20],amp3[20],amp4[20],amp5[20],amp6[20],amp7[20],amp8[20],amp9[20];
+Double_t new_tof0[20], new_tof1[20], new_tof2[20], new_tof3[20], new_tof4[20], new_tof5[20], new_tof6[20], new_tof7[20], new_tof8[20], new_tof9[20];
  //must be hardcoded if arrays are defined at file scope
-Double_t gamma_flash0[5], gamma_flash1[5], gamma_flash2[5], gamma_flash3[5], gamma_flash4[5], gamma_flash5[5], gamma_flash6[5], gamma_flash7[5], gamma_flash8[5], gamma_flash9[5];
+Double_t gamma_flash0[20], gamma_flash1[20], gamma_flash2[20], gamma_flash3[20], gamma_flash4[20], gamma_flash5[20], gamma_flash6[20], gamma_flash7[20], gamma_flash8[20], gamma_flash9[20];
 Int_t detn_all[10];
 Double_t c=299792458.0; //speed of light in m/s
 Double_t neutron_mass= 939.565420;// mev/c^2
@@ -28,38 +28,10 @@ Double_t l=185.0; // flight path in meters
 
 void gamma_flash(int run_number) {
 
-    std::string filename = "/Users/nico/Desktop/Tese/Results/anodes/coincidences_raw/threshold_400/output_run" + std::to_string(run_number) + ".root";
+    std::string filename = "/Users/nico/Desktop/Tese/Results/anodes/coincidences_raw/toy/output_run" + std::to_string(run_number) + ".root";
     TFile *file = TFile::Open(filename.c_str()) ;//we need raw data for the pickup
     TTree *intree = (TTree*)file->Get("nTOF_coincidences");
 
-    // Open the raw run118560.root file and load the PKUP tree
-    std::string filename2 = "/Users/nico/Desktop/Tese/n_TOF_data/run" + std::to_string(run_number) + ".root";
-    TFile *file2 = TFile::Open(filename2.c_str());//we need raw data for the pickup
-    TTree *pkupTree = (TTree*)file2->Get("PKUP");
-    pkupTree->SetBranchStatus("*", 0); // turn off all branches
-    pkupTree->SetBranchStatus("RunNumber", 1); // Int_t
-    pkupTree->SetBranchStatus("time", 1); // Int_t
-    pkupTree->SetBranchStatus("psTime", 1); // Double_t
-    pkupTree->SetBranchStatus("BunchNumber", 1); // Int_t
-    pkupTree->SetBranchStatus("PSpulse", 1); // Int_t
-    pkupTree->SetBranchStatus("PulseIntensity", 1); // Float_t
-    pkupTree->SetBranchStatus("detn", 1); // Int_t
-    pkupTree->SetBranchStatus("tof", 1); // Double_t
-    pkupTree->SetBranchStatus("amp", 1); // Float_t
-
-    Int_t pkup_RunNumber, pkup_time, pkup_BunchNumber, pkup_PSpulse;
-    Double_t pkup_psTime;
-    Float_t pkup_PulseIntensity;
-    Float_t amp;
-
-    pkupTree->SetBranchAddress("RunNumber", &pkup_RunNumber);
-    pkupTree->SetBranchAddress("time", &pkup_time);
-    pkupTree->SetBranchAddress("psTime", &pkup_psTime);
-    pkupTree->SetBranchAddress("BunchNumber", &pkup_BunchNumber);
-    pkupTree->SetBranchAddress("PSpulse", &pkup_PSpulse);
-    pkupTree->SetBranchAddress("PulseIntensity", &pkup_PulseIntensity);
-    pkupTree->SetBranchAddress("tof", &tof);
-    pkupTree->SetBranchAddress("amp", &amp);
 
     intree->SetBranchStatus("*", 1); // turn on all branches
     intree->SetBranchAddress("RunNumber", &RunNumber);
@@ -102,7 +74,6 @@ void gamma_flash(int run_number) {
     intree->SetBranchAddress("mult", &mult);
 
     Long64_t nentries = intree->GetEntries(); // getting the number of entries to loop over them
-    Long64_t npkupEntries = pkupTree->GetEntries();
     std::string out_file = "/Users/nico/Desktop/Tese/Results/anodes/gamma_flash/gamma_flash_" + std::to_string(run_number) + ".root";
     TFile *outfile = new TFile(out_file.c_str(), "RECREATE");
     TTree *outtree = new TTree("coincidences", "Coincidences Tree");
@@ -145,30 +116,23 @@ void gamma_flash(int run_number) {
     outtree->Branch("gamma_flash8", gamma_flash8, "gamma_flash8[mult8]/D");
     outtree->Branch("gamma_flash9", gamma_flash9, "gamma_flash9[mult9]/D");
     // now we will loop over the entries and search  the gamma flash, i.e. coincidence in all detectors
-    //we loop over pickup data and check if parameters coincide with the gamma_flash for a proper adjustment of the times
-    //we dont have to worry about tof or amp since there is only one tof and amp per bunchNumber so there will never be ambivalence
-    for (Long64_t i = 0; i < npkupEntries; i++) {
-        pkupTree->GetEntry(i);
-        for (Long64_t j = 0; j < nentries; j++) {
-            intree->GetEntry(j);
-                if (RunNumber == pkup_RunNumber && PulseIntensity == pkup_PulseIntensity && BunchNumber == pkup_BunchNumber && PSpulse == pkup_PSpulse && eventTime == pkup_time && psTime == pkup_psTime) { // they must coincide in order to compute gamma flash
-                    if (mult > 8 && mult0>0 && mult1>0 && mult2>0 && mult3>0 && mult4>0 && mult5>0 && mult6>0 && mult7>0 && mult8>0 && mult9>0) { //trigger in every detector
-                        for(int k = 0; k < mult0; k++){gamma_flash0[k] = tof0[k] - tof;} //readjusting the gamma flash
-                        for(int k = 0; k < mult1; k++){gamma_flash1[k] = tof1[k] - tof;}
-                        for(int k = 0; k < mult2; k++){gamma_flash2[k] = tof2[k] - tof;}
-                        for(int k = 0; k < mult3; k++){gamma_flash3[k] = tof3[k] - tof;}
-                        for(int k = 0; k < mult4; k++){gamma_flash4[k] = tof4[k] - tof;}
-                        for(int k = 0; k < mult5; k++){gamma_flash5[k] = tof5[k] - tof;}
-                        for(int k = 0; k < mult6; k++){gamma_flash6[k] = tof6[k] - tof;}
-                        for(int k = 0; k < mult7; k++){gamma_flash7[k] = tof7[k] - tof;}
-                        for(int k = 0; k < mult8; k++){gamma_flash8[k] = tof8[k] - tof;}
-                        for(int k = 0; k < mult9; k++){gamma_flash9[k] = tof9[k] - tof;}
-                        outtree->Fill(); 
-                        std::cout << "Filled entry " << outtree->GetEntries() << std::endl;
+    for (Long64_t j = 0; j < nentries; j++) {
+        intree->GetEntry(j);
+                if (mult > 8 && mult0>0 && mult1>0 && mult2>0 && mult3>0 && mult4>0 && mult5>0 && mult6>0 && mult7>0 && mult8>0 && mult9>0) { //trigger in every detector
+                    for(int k = 0; k < mult0; k++){gamma_flash0[k] = tof0[k];} //readjusting the gamma flash
+                    for(int k = 0; k < mult1; k++){gamma_flash1[k] = tof1[k];}
+                    for(int k = 0; k < mult2; k++){gamma_flash2[k] = tof2[k];}
+                    for(int k = 0; k < mult3; k++){gamma_flash3[k] = tof3[k];}
+                    for(int k = 0; k < mult4; k++){gamma_flash4[k] = tof4[k] ;}
+                    for(int k = 0; k < mult5; k++){gamma_flash5[k] = tof5[k] ;}
+                    for(int k = 0; k < mult6; k++){gamma_flash6[k] = tof6[k] ;}
+                    for(int k = 0; k < mult7; k++){gamma_flash7[k] = tof7[k] ;}
+                    for(int k = 0; k < mult8; k++){gamma_flash8[k] = tof8[k] ;}
+                    for(int k = 0; k < mult9; k++){gamma_flash9[k] = tof9[k] ;}
+                    outtree->Fill();
+                    std::cout << "Filled entry " << outtree->GetEntries() << std::endl;
 
-                        }
                 }
-            }
             }
 outtree->Write();
 outfile->Close();
